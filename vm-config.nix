37d18e8cd4a86d739059610654ff8146dbebc8eb
teehee <dynamTypedDisaster@proton.me>
2025-04-25 15:49:00 +1000

    bash installed in vm for ease of use. Hawiki currently has to be started within vm and then nginx and mysql available

--- vm-config.nix
+++ vm-config.nix
@@ -3,8 +3,21 @@
   # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/qemu-vm.nix
   imports = ["${modulesPath}/virtualisation/qemu-vm.nix"];
 
+  environment.etc."fake-pass.txt".text = "troubleshooting";
   services.hawiki.enable = true;
   services.hawiki.passFile = "/etc/fake-pass.txt";
 
+  # bash commands so I can actually navigate a little in the vm
+  environment.systemPackages = [
+    pkgs.bash
+    pkgs.curl
+    pkgs.coreutils
+    pkgs.git
+    pkgs.nginx
+    pkgs.vim
+  ];
+  
   users.users.root.initialPassword = "root";
+  users.users.root.shell = pkgs.bashInteractive;
 }
+
